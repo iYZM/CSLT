f7457e5e6de7bd93ff3b19c2e921c9c9ffa451aa
AbdUllaH M <68979073+iYZM@users.noreply.github.com>
2024-12-28 01:18:20 +0200

    Update MN.cpp

--- MN.cpp
+++ MN.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main(){
-    int i, n, x, max = 0;
+    int i, n, x;
+    int max ;
     cout << "Enter The Number Of Numbers: " << endl;
     cin >> n;
     cout << "Enter The " << n << " Numbers: "<< endl;
-    for (i = 1; i <= n; i++)
+    cin >>x;
+    max =x;
+    for (i = 2; i <= n; i++)
     {
         cin >> x;
         if (x > max)
